2f078e8cb871caec04b99e3770148ca06ec8c141
Quentin Le Dilavrec <ledilavrecquentin@gmail.com>
2022-07-27 12:03:35 +0200

    better simple bench run

--- run_benchmark_simp.sh
+++ run_benchmark_simp.sh
@@ -5,10 +5,10 @@ set -e
 # Launch impact analysis on the INRIA/spoon repository.
 SHORTNAME=spoon
 REPO=INRIA/spoon
+BEFORE=d44759888f41e67db3f32ea2cd4975cefa727691
+AFTER=56e12a0c0e0e69ea70863011b4f4ca3305e0542b
 
-# run the full benchmark
-# WARNING it will take a very long time!
-# You should first try run_benchmark_simp.sh and look at the result then if it looks right run this benchmark on a server.
+# run the benchmark on a commit slice of spoon
 
 mkdir -p results_simp/
 mkdir -p results_simp/modules
@@ -20,16 +20,13 @@ mkdir -p logs_simp/
 
 # For each repository, first construct the HyperAST with indexed references (reference oracles made with bloom filters),
 # then for a sample of commits, for each declaration find all corresponding references.
-# target/release/rusted_gumtree_benchmark "$REPO" d44759888f41e67db3f32ea2cd4975cefa727691 56e12a0c0e0e69ea70863011b4f4ca3305e0542b "" results_simp/$SHORTNAME &> logs_simp/$SHORTNAME
+target/release/rusted_gumtree_benchmark "$REPO" "$BEFORE" "$AFTER" "" results_simp/$SHORTNAME &> logs_simp/$SHORTNAME
 
 # extract commits and modules that had their declarations resolved.
-# TODO
 target/release/ref-mining-evaluation modules --refs results_simp/$SHORTNAME > ../results_simp/modules/$SHORTNAME 2> /dev/null
 
 # do the same reference analysis with Spoon
 mkdir -p /tmp/spoongitinstances
-mkdir -p /tmp/spoongitinstances/$SHORTNAME
-
 (
 cd /tmp/spoongitinstances
 
@@ -49,21 +46,17 @@ cat ../hyperAST/modules/$SHORTNAME | bash ana.sh /tmp/spoongitinstances/$SHORTNA
 )
 
 # extract performances measurments on construction
-# TODO
 target/release/ref-mining-evaluation multi-perfs-stats --json ../refsolver/comp_simp/$SHORTNAME/ results_simp/$SHORTNAME/ > ../results_simp/perfs_$SHORTNAME.json 2> /dev/null
 
 
 
 # extract and compare reference relations found with the HyperAST to the one found with Spoon.
-# TODO
 target/release/ref-mining-evaluation multi-compare-stats --json ../refsolver/comp_simp/$SHORTNAME/ results_simp/$SHORTNAME/ > ../results_simp/summary_$SHORTNAME.json 2> /dev/null
 
 # display some stats on results
-# TODO
 ls ../results_simp/
 
 # a dedicated notebook is available to plot results
-# TODO
 cd ../observable_notebook_results
 if ! command -v npm &> /dev/null
 then
